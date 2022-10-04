f0954815b6f0673196819b2dc6cc9b2434f125ef
rajankhadka <rajankhadka.infodeveloper@gmail.com>
2022-10-04 13:25:41 +0545

    feat(sort): solved quick sort and merge sort

--- problem/sort/merge-sort.problem.cpp
+++ problem/sort/merge-sort.problem.cpp
@@ -7,13 +7,49 @@
 
 using namespace std;
 
-vector<int> mergeSort(vector<int> arr){
+void merge(vector<int> &arr, int start, int mid, int end){
+    int i = start, j = mid+1, k=0;
+    int *temp = (int*)malloc((end-start+1)*sizeof(int));
+    while(i<=mid && j<=end){
+        if(arr[i] <= arr[j]) temp[k++] = arr[i++];
+        else temp[k++] = arr[j++];
+    }
 
-    return arr;
+    while(i<=mid) temp[k++]= arr[i++];
+    while(j<=end) temp[k++] = arr[j++];
+
+    for(i=start;i<=end;i++){
+        arr[i] = temp[i-start];
+    }
+    delete[] temp;
+}
+
+void divide(vector<int> &arr, int start, int end){
+    if(start< end){
+        int mid = int((start+end)/2);
+        divide(arr,start,mid);
+        divide(arr,mid+1,end);
+        merge(arr,start,mid,end);
+    }
+}
+
+vector<int> mergeSort(vector<int> array) {
+    // Write your code here.
+    size_t end = array.size();
+    divide(array,0,end-1);
+    return array;
 }
 
 int main(){
     vector<int> data{4,1,5,2,-1,3,0,6};
-    cout << "merge";
+    for(const int &i:data){
+        cout << i << "   ";
+    }
+    cout << endl;
+    data = mergeSort(data);
+    for(const int &i:data){
+        cout << i << "   ";
+    }
+    cout << endl;
     return 0;
 }
--- problem/sort/quick-sort.problem.cpp
+++ problem/sort/quick-sort.problem.cpp
@@ -6,6 +6,43 @@
 
 using namespace std;
 
+void swap(vector<int> &arr, int firstIdx, int secondIdx){
+    int temp = arr[firstIdx];
+    arr[firstIdx] = arr[secondIdx];
+    arr[secondIdx] = temp;
+}
+
+int partition(vector<int> &arr, int start, int end){
+    int pIdx = start, pValue = arr[end];
+    for(int i=start;i<end;i++){
+        if(arr[i] < pValue){
+            swap(arr,i,pIdx);
+            pIdx++;
+        }
+    }
+    swap(arr,pIdx,end);
+    return pIdx;
+}
+
+void quickSort(vector<int> &arr,int start, int end){
+    if(start >= end) return;
+    //find pivot;
+    int pIdx = partition(arr,start,end);
+    quickSort(arr, start,pIdx-1);
+    quickSort(arr, pIdx+1, end);
+}
+
+
 int main(){
-    cout << "hello";
+    vector<int> data{4,1,5,2,-1,3,0,6};
+//    cout << data.size();
+    for(const int &i:data){
+        cout << i << "   ";
+    }
+    cout << endl;
+    quickSort(data, 0, data.size()-1);
+    for(const int &i:data){
+        cout << i << "   ";
+    }
+    cout << endl;
 }
